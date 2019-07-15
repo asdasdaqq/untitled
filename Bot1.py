c9d014cbb5b1e2d6f675aabbeaca485064a7b284
R <sibiozik@yahoo.com>
2019-07-15 15:06:32 +0500

    Добавил ответ на сообщение таким же сообщением[1]

--- Bot1.py
+++ Bot1.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler
 import requests
 import re
 
