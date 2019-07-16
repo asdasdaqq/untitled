6df4f09a9dcea252ac9d125aafaab388f1a1924d
R <sibiozik@yahoo.com>
2019-07-16 14:43:00 +0500

    По-другому добавил ответ на сообщение таким же сообщением[3].

--- Bot1.py
+++ Bot1.py
@@ -38,7 +38,8 @@ def cat(bot, update):
 
 def listen(bot,update):
     chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text=update.message.text)
+    bot.send_message(chat_id=chat_id, text=chat_id)
+
 
 
 def main():
