be667693c2b82ef7daeed739e73387304964e498
R <sibiozik@yahoo.com>
2019-07-25 17:01:22 +0500

    Добавил отправку рецептов еды.

--- Bot1.py
+++ Bot1.py
@@ -1,6 +1,9 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters
 import requests
 import re
+import random
+from lxml import html
+from bs4 import BeautifulSoup
 
 
 def get_url(api):
@@ -41,12 +44,33 @@ def listen(bot,update):
     bot.send_message(chat_id=chat_id, text=update.message.text)
     bot.forward_message(chat_id=230512694,from_chat_id=chat_id,message_id=update.message.message_id)
 
+def food(bot,update):
+    recipe_link = []
+    url = 'https://www.yummly.com/recipes'
+    r = requests.get(url)
+    with open('test.html', 'w', encoding='utf-8') as output_file:
+        output_file.write(r.text)
+
+    with open('test.html', encoding='utf-8') as input_file:
+        text = input_file.read()
+
+    soup = BeautifulSoup(text, features='lxml')
+    recipe_list = soup.find_all('div', {'class': 'recipe-card-img'})
+    for item in recipe_list:
+        recipe_link.append(item.get('data-pin-url'))
+
+    answ=recipe_link[random.randint(0, len(recipe_link))]
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=chat_id, text='Вот тебе рецепт чего-то вкусненького')
+    bot.send_message(chat_id=chat_id, text=answ)
+
 
 def main():
     updater = Updater('869686653:AAGXT2cDZHEDpGZ0u67YOxag7pP0YmpvtWw')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('dog', dog))
     dp.add_handler(CommandHandler('cat', cat))
+    dp.add_handler(CommandHandler('food', food))
     dp.add_handler(MessageHandler(Filters.text,listen))
     updater.start_polling()
     updater.idle()
